8a5b1952d5d7c615549244b347b1e3ba826727a8
Nils Konradsson <niko0416@storsjogymnasiet.se>
2024-09-17 15:11:41 +0200

    Did things

--- Librarby/book_generator.py
+++
@@ -1,6 +0,0 @@
-from work import *
-
-work_list = {
-    "books": [Nonfiction("Travelling at Night, Vol. 1", 1923, False, "The Malleary", True, None, "Christopher Illopoly", 1, "Nyctrodomy")],
-    "audiobooks": [Audiobook("Travelling at Night, Vol. 1", 2011, False, "The Haustorum", True, "Maltese Audiobooks Inc.", None, "Maltese Man")]
-}
--- Librarby/main.py
+++ Librarby/main.py
@@ -1,6 +1,14 @@
 from work import *
-from book_generator import work_list
 import os
+import atexit
+import pickle
+import sys
+
+work_list = {
+    "books": [Nonfiction("Travelling at Night, Vol. 1", 1923, False, "The Malleary", True, None, "Christopher Illopoly", 1, "Nyctrodomy")],
+    "audiobooks": [Audiobook("Travelling at Night, Vol. 1", 2011, False, "The Haustorum", True, "Maltese Audiobooks Inc.", None, "Maltese Man")],
+    "hentai": [Hentai("Metamorphosis", 2013, False, "Hell", True, "Pain", "Art", "FAKKU", False)]
+}
 
 def main():
     clear()
@@ -13,7 +21,9 @@ def main():
             index += 1
         try:
             choice = int(input())
-        except:
+        except Exception as e:
+            if type(e) == KeyboardInterrupt:
+                sys.exit()
             main()
     else:
         choice = 1
@@ -25,11 +35,13 @@ def view_category(category):
     print(f"List of {list(work_list.keys())[list(work_list.values()).index(category)]}:")
     index = 1
     for i in category:
-        print(f"{index} - {i.title.capitalize()}")
+        print(f"{index} - {i.title}")
         index += 1
     try:
         choice = int(input())
-    except:
+    except Exception as e:
+        if type(e) == KeyboardInterrupt:
+            sys.exit()
         view_category(category)
 
     view_work(category[choice - 1])
@@ -38,14 +50,66 @@ def view_work(work):
     clear()
     properties = vars(work)
     print(properties["title"])
-    del properties["title"]
     for i in list(properties.keys()):
+        if i == "title": continue
         print(f"{i.replace('_', ' ').capitalize()} - {properties[i]}")
-    
-    input()
+
+    cur_cat = None
+    cur_index = 0
+    for i in list(work_list.keys()):
+        cur_index = 0
+        cur_cat = i
+        for j in work_list[i]:
+            if j == work:
+                break
+            cur_index += 1
+        else: continue
+        break
+
+    if work.in_library:
+        print("\n1 - Borrow")
+
+        try:
+            assert int(input()) == 1
+        except Exception as e:
+            if type(e) == KeyboardInterrupt:
+                sys.exit()
+            main()
+
+        work_list[cur_cat][cur_index].borrow()
+        if Book.read in dir(work):
+            work.read()
+        else:
+            print(f"{work.title} has been borrowed.")
+        input()
+
+    else:
+        print("\nWork is borrowed\n1 - Restore")
+
+        try:
+            assert int(input()) == 1
+        except Exception as e:
+            if type(e) == KeyboardInterrupt:
+                sys.exit()
+            main()
+
+        work_list[cur_cat][cur_index].restore()
+        input(f"{work.title} has been restored.\n")
+
     main()
 
 def clear():
     os.system("cls" if os.name == "nt" else "clear")
 
+def save_state():
+    with open("books.dat", "wb") as file:
+        file.write(pickle.dumps(work_list))
+
+def load_works():
+    global work_list
+    with open("books.dat", "rb") as file:
+        work_list = pickle.loads(file.read())
+
+atexit.register(save_state)
+load_works()
 main()
